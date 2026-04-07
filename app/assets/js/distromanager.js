a5297d6a50c57ace78f6cc5e31b38d0fcc13e4f5
Alo3e <kirillchub@mail.ru>
2026-04-07 18:09:32 +0300

    temp ru-files redirect

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,9 +2,7 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-// Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/Alo3-gh/AwesomeCraftNebula/main/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://ru-files.awesome-craft.ru/launcher/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
